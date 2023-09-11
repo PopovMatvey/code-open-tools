f3347880358d461575dbabca7f29c02f47f15164
матвей попов <matyha550@gmail.com>
2023-09-11 15:43:47 +0300

    code open tools v 0.0.1

---
+++ README.md
@@ -0,0 +1,2 @@
+# code-open-tools
+Примеры реализации различных фич с code open
---
+++ clear-js/hamburger-menu-animations/js/script.js
@@ -0,0 +1,28 @@
+Array.from(document.getElementsByTagName('path')).map(path => {
+    const debugPath = path.cloneNode();
+
+    debugPath.classList.add('line--debug');
+    if (path.parentNode) path.parentNode.insertBefore(debugPath.cloneNode(), path);
+  });
+
+  const debugCheckbox = document.getElementById('debug');
+
+  debugCheckbox.addEventListener('change', () => {
+    if (debugCheckbox.checked) {
+      debugCheckbox.parentElement.classList.add('active');
+    } else {
+      debugCheckbox.parentElement.classList.remove('active');
+    }
+  });
+
+  let currentActive = 0;
+  const checkboxes = document.querySelectorAll('.grid input');
+
+  const autoShow = setInterval(() => {
+    checkboxes[currentActive % 4].checked = !checkboxes[currentActive % 4].checked;
+    if (!checkboxes[currentActive % 4].checked) currentActive += 1;
+  }, 1000);
+
+  document.querySelector('.grid').addEventListener('click', () => {
+    clearInterval(autoShow);
+  })
